0e62c3095fc17f0849bdb8f1b4b6576938a89ef7
Abdelrahman Ahmed <76922586+AbdelrahmanElEkiaby@users.noreply.github.com>
2024-06-05 20:26:31 +0300

    changes

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,240 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Unity.Netcode;
-//using TMPro;
-//using Unity.Services.Lobbies.Models;
-//using System;
-
-//public class GameManager : NetworkBehaviour
-//{
-//    public static GameManager Instance { get; private set; }
-
-//    public NetworkVariable<float> timer = new NetworkVariable<float>();
-//    [SerializeField] private TMP_Text timerText;
-//    [SerializeField] GameObject EndingHUD;
-//    [SerializeField] private TMP_Text EndingText;
-//    private bool OnStart;
-
-//    private int playersConnected;
-
-//    private void Awake()
-//    {
-//        // Singleton pattern to ensure only one instance of GameManager
-//        if (Instance == null)
-//        {
-//            Instance = this;
-//        }
-//        else
-//        {
-//            Destroy(gameObject);
-//        }
-//    }
-
-//    void Start()
-//    {
-//        if (IsServer)
-//        {
-//            timer.Value = 60;
-//        }
-//        if (IsServer)
-//        {
-//            if (NetworkManager.Singleton != null)
-//            {
-//                NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-//                NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
-//            }
-//        }
-
-//    }
-
-//    private void OnClientConnected(ulong clientId)
-//    {
-//        if (!IsServer) return;
-
-//        // Increment the number of connected players
-//        playersConnected++;
-
-//        //Debug.Log(playersConnected);
-//        // Check if two players are connected
-//        if (playersConnected == 1)
-//        {
-//            OnStart = true;
-//            StartGame();
-//        }
-//    }
-
-//    private void OnClientDisconnected(ulong clientId)
-//    {
-//        if (!IsServer) return;
-
-//        // Decrement the number of connected players
-//        playersConnected--;
-//        if (playersConnected == 0)
-//        {
-//            OnStart = false;
-//            StopGame();
-//        }
-//    }
-
-//    void Update()
-//    {
-//        if (!IsServer) { return; }
-//        if (OnStart)
-//        {
-
-//            if ((int)timer.Value == 55)
-//            {
-//                ActivateMeter();
-//            }
-//            //else if ((int)timer.Value == 50)
-//            //{
-//            //    DeactivateMeter();
-//            //}
-//            timer.Value -= Time.deltaTime;
-//            UpdateTimerDisplay();
-//            UpdateTimerClientRpc();
-//        }
-
-//    }
-
-//    public override void OnNetworkSpawn()
-//    {
-//        if (!IsServer) { return; }
-//    }
-
-//    public override void OnNetworkDespawn()
-//    {
-//        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-//        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
-//    }
-
-//    public void OnPlayerDied(ulong playerId)
-//    {
-//        if (!IsServer) return;
-//        ulong winnerId = GetWinner(playerId);
-//        UserData userData =
-//                HostSingleton.Instance.GameManger.networkServer.GetUserDataByClientId(winnerId);
-//        string winner = userData.userName;
-//        DisplayWinnerClientRpc(winner);
-//        StopGame();
-//        OnStart = false;
-//    }
-
-//    private ulong GetWinner(ulong deadPlayerId)
-//    {
-//        foreach (var client in NetworkManager.Singleton.ConnectedClients)
-//        {
-//            if (client.Key != deadPlayerId)
-//            {
-//                return client.Key;
-//            }
-//        }
-//        return ulong.MaxValue; // No winner found
-//    }
-
-//    [ClientRpc]
-//    private void DisplayWinnerClientRpc(string winner)
-//    {
-//        EndingHUD.SetActive(true);
-//        EndingText.text = $"{winner} won";
-
-//    }
-
-//    [ClientRpc]
-//    private void UpdateTimerClientRpc()
-//    {
-//        UpdateTimerDisplay();
-//    }
-
-//    private void UpdateTimerDisplay()
-//    {
-//        float minutes = Mathf.FloorToInt(timer.Value / 60);
-//        float seconds = Mathf.FloorToInt(timer.Value % 60);
-//        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-//    }
-
-//    private void StartGame()
-//    {
-//        foreach (var client in NetworkManager.Singleton.ConnectedClients)
-//        {
-//            StartGameClientRpc(client.Key);
-//        }
-//    }
-
-//    private void StopGame()
-//    {
-//        foreach (var client in NetworkManager.Singleton.ConnectedClients)
-//        {
-//            StopGameClientRpc(client.Key);
-//        }
-//    }
-
-//    private void ActivateMeter()
-//    {
-//        foreach (var client in NetworkManager.Singleton.ConnectedClients)
-//        {
-//            ActivateMeterClientRpc(client.Key);
-//        }
-//    }
-
-//    [ClientRpc]
-//    private void ActivateMeterClientRpc(ulong clientId)
-//    {
-//        if (NetworkManager.Singleton.LocalClientId == clientId)
-//        {
-//            if (NetworkManager.Singleton.LocalClient.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
-//            {
-//                playerScript.ActivateMeter();
-//            }
-//        }
-//    }
-
-//    private void DeactivateMeter()
-//    {
-//        foreach (var client in NetworkManager.Singleton.ConnectedClients)
-//        {
-//            DeactivateMeterClientRpc(client.Key);
-//        }
-//    }
-
-//    [ClientRpc]
-//    private void DeactivateMeterClientRpc(ulong clientId)
-//    {
-//        if (NetworkManager.Singleton.LocalClientId == clientId)
-//        {
-//            if (NetworkManager.Singleton.LocalClient.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
-//            {
-//                playerScript.DeactivateMeter();
-//            }
-//        }
-//    }
-
-//    [ClientRpc]
-//    private void StopGameClientRpc(ulong clientId)
-//    {
-//        if (NetworkManager.Singleton.LocalClientId == clientId)
-//        {
-//            if (NetworkManager.Singleton.LocalClient.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
-//            {
-//                playerScript.StopMovement();
-//            }
-//        }
-//    }
-
-//    [ClientRpc]
-//    private void StartGameClientRpc(ulong clientId)
-//    {
-//        if (NetworkManager.Singleton.LocalClientId == clientId)
-//        {
-//            if (NetworkManager.Singleton.LocalClient.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
-//            {
-//                playerScript.StartMovement();
-//            }
-//        }
-//    }
-//}
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -248,10 +11,13 @@ public class GameManager : NetworkBehaviour
     public static GameManager Instance { get; private set; }
 
     public NetworkVariable<float> timer = new NetworkVariable<float>();
+    public NetworkVariable<float> CountDowntimer = new NetworkVariable<float>();
     [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text CountDownTimerText;
     [SerializeField] GameObject EndingHUD;
     [SerializeField] private TMP_Text EndingText;
-    private bool OnStart;
+    private bool OnStart = false;
+    private bool CountDownStart;
 
     private int playersConnected;
 
@@ -273,6 +39,8 @@ public class GameManager : NetworkBehaviour
         if (IsServer)
         {
             timer.Value = 60;
+            CountDowntimer.Value = 6;
+            //timerText.gameObject.SetActive(false);
         }
         if (IsServer)
         {
@@ -291,11 +59,12 @@ public class GameManager : NetworkBehaviour
         // Increment the number of connected players
         playersConnected++;
 
-        // Check if two players are connected
+        // Check if a certain number of players are connected
         if (playersConnected == 1)
         {
+            CountDownStart = true;
             OnStart = true;
-            StartGame();
+            SetCountDownTimerActiveClientRpc(true);
         }
     }
 
@@ -315,20 +84,70 @@ public class GameManager : NetworkBehaviour
     void Update()
     {
         if (!IsServer) { return; }
-        if (OnStart)
+
+        if (CountDownStart)
         {
-            if ((int)timer.Value == 55)
+            if((int)CountDowntimer.Value == 2)
             {
-                ActivateMeter();
+                ChangeTextColorClientRpc();
             }
-            //else if ((int)timer.Value == 50)
-            //{
-            //    DeactivateMeter();
-            //}
+            if ((int)CountDowntimer.Value == 0)
+            {
+                CountDownStart = false;
+                SetCountDownTimerActiveClientRpc(false);
+                SetTimerTextActiveClientRpc(true);
+                StartGame();
+            }
+            else
+            {
+                CountDowntimer.Value -= Time.deltaTime;
+                UpdateCountDownDisplayClientRpc();
+            }
+        }
+        if (OnStart && !CountDownStart)
+        {
+            if((int)timer.Value == 55)
+            {
+                ActivateScoreCalcClientRpc();
+            }
+            else if((int)timer.Value == 50)
+            {
+                DeactivateScoreCalcClientRpc();
+            }
             timer.Value -= Time.deltaTime;
             UpdateTimerDisplay();
             UpdateTimerClientRpc();
         }
+    }
+
+    [ClientRpc]
+    private void ChangeTextColorClientRpc()
+    {
+        CountDownTimerText.color = Color.red;
+    }
+
+    [ClientRpc]
+    void UpdateCountDownDisplayClientRpc()
+    {
+        UpdateCountDownDisplay();
+    }
+
+    void UpdateCountDownDisplay()
+    {
+        float seconds = Mathf.FloorToInt(CountDowntimer.Value % 60);
+        CountDownTimerText.text = $"{seconds}";
+    }
+
+    [ClientRpc]
+    private void SetCountDownTimerActiveClientRpc(bool isActive)
+    {
+        CountDownTimerText.gameObject.SetActive(isActive);
+    }
+
+    [ClientRpc]
+    private void SetTimerTextActiveClientRpc(bool isActive)
+    {
+        timerText.gameObject.SetActive(isActive);
     }
 
     public override void OnNetworkSpawn()
@@ -458,7 +277,32 @@ public class GameManager : NetworkBehaviour
             }
         }
     }
+
+    [ClientRpc]
+    private void ActivateScoreCalcClientRpc()
+    {
+        foreach (var client in NetworkManager.Singleton.ConnectedClients)
+        {
+            if (client.Value.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
+            {
+                playerScript.ActivateCounter();
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void DeactivateScoreCalcClientRpc()
+    {
+        foreach (var client in NetworkManager.Singleton.ConnectedClients)
+        {
+            if (client.Value.PlayerObject.TryGetComponent(out ScaleFromMicrophone playerScript))
+            {
+                playerScript.DeactivateCounter();
+            }
+        }
+    }
 }
+
 
 
 
--- Assets/Scripts/Player/ScaleFromMicrophone.cs
+++ Assets/Scripts/Player/ScaleFromMicrophone.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScaleFromMicrophone : NetworkBehaviour
 {
     [SerializeField] AudioLoudnessDetector detector;
     [SerializeField] Image soundMeter;
+    [SerializeField] TMP_Text ScreamTotalText;
     Rigidbody2D rb;
     public bool isStart = false;
 
+
+    public NetworkVariable<float> TotalScreamScore = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     public NetworkVariable<bool> MeterActive = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     public NetworkVariable<float> loudnessValue = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    public NetworkVariable<bool> CounterActive = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     public float LoudnessSensitivity = 100f;
     public float threshold = 0.1f;
@@ -31,6 +36,12 @@ public class ScaleFromMicrophone : NetworkBehaviour
 
         float loudness = detector.GetLoudnessFromMicrophone() * LoudnessSensitivity * threshold;
         loudnessValue.Value = loudness;  // Update the network variable
+        
+        if(MeterActive.Value == true)
+        {
+            TotalScreamScore.Value += loudness;
+            DisplayScreamScoreClientRpc();
+        }
 
         Vector2 playerVelocity = new Vector2(rb.velocity.x, rb.velocity.y + loudness - gravity);
         rb.velocity = playerVelocity;
@@ -38,14 +49,14 @@ public class ScaleFromMicrophone : NetworkBehaviour
 
     private void Update()
     {
-        if (MeterActive.Value)
-        {
-            soundMeter.fillAmount = loudnessValue.Value;  // Update the UI for all clients when MeterActive is true
-        }
-        else
-        {
-            soundMeter.fillAmount = 0;  // Ensure the meter is set to 0 when MeterActive is false
-        }
+        //if (MeterActive.Value)
+        //{
+        //    soundMeter.fillAmount = loudnessValue.Value;  // Update the UI for all clients when MeterActive is true
+        //}
+        //else
+        //{
+        //    soundMeter.fillAmount = 0;  // Ensure the meter is set to 0 when MeterActive is false
+        //}
     }
 
     public void StartMovement()
@@ -95,5 +106,36 @@ public class ScaleFromMicrophone : NetworkBehaviour
             MeterActive.Value = false;
         }
         soundMeter.fillAmount = 0;
+    }
+
+    public void ActivateCounter()
+    {
+        if (!IsOwner) return;
+        ActivateCounterClientRpc();
+    }
+
+    public void DeactivateCounter()
+    {
+        if (!IsOwner) return;
+        DeactivateCounterClientRpc();
+    }
+
+    [ClientRpc]
+    private void ActivateCounterClientRpc()
+    {
+        CounterActive.Value = true;
+        ScreamTotalText.gameObject.SetActive(true);
+    }
+
+    [ClientRpc]
+    private void DeactivateCounterClientRpc()
+    {
+        CounterActive.Value = false ;
+    }
+
+    [ClientRpc]
+    private void DisplayScreamScoreClientRpc()
+    {
+        ScreamTotalText.text = $"{TotalScreamScore.Value}";
     }
 }
